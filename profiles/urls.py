5ad67a6e62a29f8f81a535b52cc73e0437068410
Joke Heyndels <jo_hannah@outlook.com>
2021-09-16 14:21:23 +0000

    Add profile detail view with update form

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProfileList
+from .views import ProfileList, ProfileDetail
 
 
 urlpatterns = [
-    path("profiles/", ProfileList.as_view())
+    path("profiles/", ProfileList.as_view()),
+    path("profiles/<int:pk>", ProfileDetail.as_view())
 ]
