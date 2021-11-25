3558218b515f931b7d3e6c2de052fd880893d99c
Joke Heyndels <jo_hannah@outlook.com>
2021-11-25 16:15:40 +0000

    commit-01

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+# DEBUG = "DEVELOPMENT" in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
     os.environ.get("ALLOWED_HOST"),
