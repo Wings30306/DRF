056f63086ef5f75906cb8c749e4d5a9ac708564a
Joke Heyndels <jo_hannah@outlook.com>
2022-01-06 09:09:15 +0000

    Update likes model to include related name

--- likes/models.py
+++ likes/models.py
@@ -11,7 +11,9 @@ class Like(models.Model):
     'UniqueConstraint' makes sure a user can't like the same post twice.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name="likes", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
