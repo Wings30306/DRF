bd5d92968c2cdaf4bc4cba896565d011f0503edc
Joke Heyndels <jo_hannah@outlook.com>
2022-10-17 11:04:25 +0000

    Include RENDER_EXTERNAL_HOSTNAME in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,9 +63,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get("ALLOWED_HOST"),
     'localhost',
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
