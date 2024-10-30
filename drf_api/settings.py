543cce37bcc8c959fecfeaf095cae2874535f26d
Joke Heyndels <jo_hannah@outlook.com>
2024-10-30 08:59:44 +0000

    Update CORS_ALLOWED_ORIGINS_REGEXES for codeinstitute-ide.net

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -116,7 +116,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        r"^https:\/\/.*\.codeinstitute-ide\.net$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
